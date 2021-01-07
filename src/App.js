59c0fd106406e5bff6f300f801cb8746baa8a0ec
Luka Juric <lukajuric98765@gmail.com>
2021-01-07 22:19:17 +0100

    created functions component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,7 @@ import React from 'react';
 import './App.css';
 import Messages from "./Messages";
 import Input from "./Input";
-
-
-function randomName() {
-  const name = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
-  const randomName = name[Math.floor(Math.random() * name.length)];
-  return randomName;
-}
-
-function randomEmoji() {
-  const possibleEmojis = [
-    '🐀','🐁','🐭','🐹','🐂','🐃','🐄','🐮','🐅','🐆','🐯','🐇','🐐','🐑','🐏','🐴',
-    '🐎','🐱','🐈','🐰','🐓','🐔','🐤','🐣','🐥','🐦','🐧','🐘','🐩','🐕','🐷','🐖',
-    '🐗','🐫','🐪','🐶','🐺','🐻','🐨','🐼','🐵','🙈','🙉','🙊','🐒','🐉','🐲','🐊',
-    '🐍','🐢','🐸','🐋','🐳','🐬','🐙','🐟','🐠','🐡','🐚','🐌','🐛','🐜','🐝','🐞',
-  ];
-  const randomEmoji = Math.floor(Math.random() * possibleEmojis.length);
-  return possibleEmojis[randomEmoji];
-}
+import {randomName, randomEmoji} from "./Functions";
 
 class App extends React.Component {
   constructor() {
